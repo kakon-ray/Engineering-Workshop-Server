4190a8a1e654c2a797ca70fc114df74a431c878d
Kakon Ray <thisiskakonray@gmail.com>
2022-05-24 18:23:54 +0600

    admin manage order api create

--- index.js
+++ index.js
@@ -79,6 +79,15 @@ async function run() {
       res.send(result);
     });
 
+    // delete prodcut to product collection
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const deleteOrder = await productCollection.deleteOne(query);
+      res.send(deleteOrder);
+    });
+
     // get one user information
     app.get("/user/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
@@ -111,6 +120,13 @@ async function run() {
       const cursor = purchesCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    app.delete("/allorder/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const deleteOrder = await purchesCollection.deleteOne(query);
+      res.send(deleteOrder);
     });
 
     // get order to myorder route frontend
